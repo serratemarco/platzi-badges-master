ec1bd7244967891a822abd1242b92f8d043f2a39
Marco Serrate <serratemarco@gmail.com>
2020-07-07 16:02:03 -0400

    Creamos los estados de los valores de las variables y los manejamos con state y setstate\

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,83 +1,94 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
-  handleClick = e => {
-    console.log('Button was clicked');
+
+//inicializamos el estado
+
+state ={
+  jobTitle: 'Designer',
+};
+
+  handleChange =e =>{
+    // console.log({
+    //   name: e.target.name,
+    //   value: e.target.value});
+  
+    //Utilizo un componente de la clase component para verificar los estados de las variables
+    this.setState({
+      [e.target.name]: e.target.value,
+      
+    })
+
+  };
+  handleClick =e =>{
+    console.log("Button was clicked");
   };
 
-  handleSubmit = e => {
+  handleSubmit =e =>{
     e.preventDefault();
-    console.log('Form was submitted');
-    console.log(this.state);
+    console.log("Form was submit");
+    console.log(this.state)
   };
 
   render() {
     return (
       <div>
         <h1>New Attendant</h1>
-
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label>First Name</label>
             <input
-              onChange={this.props.onChange}
+              onChange={this.handleChange}
               className="form-control"
               type="text"
               name="firstName"
-              value={this.props.formValues.firstName}
+              value={this.state.firstName}
             />
           </div>
-
           <div className="form-group">
             <label>Last Name</label>
             <input
-              onChange={this.props.onChange}
+              onChange={this.handleChange}
               className="form-control"
               type="text"
               name="lastName"
-              value={this.props.formValues.lastName}
+              value={this.state.lastName}
             />
           </div>
-
           <div className="form-group">
             <label>Email</label>
             <input
-              onChange={this.props.onChange}
+              onChange={this.handleChange}
               className="form-control"
               type="email"
               name="email"
-              value={this.props.formValues.email}
+              value={this.state.email}
             />
           </div>
-
           <div className="form-group">
             <label>Job Title</label>
             <input
-              onChange={this.props.onChange}
+              onChange={this.handleChange}
               className="form-control"
               type="text"
               name="jobTitle"
-              value={this.props.formValues.jobTitle}
+              value={this.state.jobTitle}
             />
           </div>
-
           <div className="form-group">
             <label>Twitter</label>
             <input
-              onChange={this.props.onChange}
+              onChange={this.handleChange}
               className="form-control"
               type="text"
               name="twitter"
-              value={this.props.formValues.twitter}
+              value={this.state.twitter}
             />
           </div>
-
-          <button onClick={this.handleClick} className="btn btn-primary">
-            Save
-          </button>
+        <button onClick={this.handleClick} className="btn btn-primary">Save</button>
         </form>
       </div>
-    );
+    )
   }
 }
 
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,6 +3,7 @@ import './styles/BadgeNew.css';
 import headers from '../images/badge-header.svg'
 import Navbar from '../components/Navbar';
 import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
 class BadgeNew extends React.Component{
   render(){
     return(
@@ -13,7 +14,7 @@ class BadgeNew extends React.Component{
         </div>
         <div className="container">
         <div className="row">
-          <div className="col">
+          <div className="col-6">
             <Badge
              firstName="Marco"
              lastName="Serrate"
@@ -21,6 +22,9 @@ class BadgeNew extends React.Component{
             jobTitle="Front Enginner"
             avatarUrl="https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
             />
+          </div>
+          <div className ="col-6">
+            <BadgeForm/>
           </div>
         </div>
         </div>
