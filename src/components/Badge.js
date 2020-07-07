89d45922fd22772459b974ddddd704af39c2b986
Marco Serrate <serratemarco@gmail.com>
2020-07-06 22:31:00 -0400

    Hicimos la primera parte de nuestra pagina, para ser exacto, creamos un nuevo BadgeNew y llamamos a nuestro Badge luego creamos nuestro navbar dentro de BadgeNew

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -5,21 +5,27 @@ import "./styles/Badge.css";
 
 class Badge extends React.Component{
   render(){
-    return <div className="Badge">
+    // const firstname='Mark';
+    // const lastname='Serrate';
+    // const nikname='@serratemarco';
+
+    
+
+return <div className="Badge">
       <div className="Badge__header">
         <img src={confLogo} alt="Logo conferencia platzi"/>
       </div>
       <div className="Badge__section-name">
         <img className="Badge__avatar"
          src="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon" alt="avatar"/>
-        <h1>Marco<br/>Serrate</h1>
+        <h1>{this.props.firstName}<br/>{this.props.lastName}</h1>
       </div>
       <div className="Badge__section-info">
-        <h3>Frontend React.js</h3>
-        <div>@serratemar</div>
+      <h3>{this.props.jobTitle}</h3>
+        <div>@{this.props.twiter}</div>
       </div>   
       <div className="Badge__footer">
-      <p>#platziConf</p> 
+        <p>#platziConf</p> 
       </div>
     </div>
   }
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,60 +1,31 @@
 import React from 'react';
-
 import './styles/BadgeNew.css';
-import header from '../images/badge-header.svg';
+import headers from '../images/badge-header.svg'
+import Navbar from '../components/Navbar';
 import Badge from '../components/Badge';
-import BadgeForm from '../components/BadgeForm';
-
-class BadgeNew extends React.Component {
-  state = {
-    form: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      jobTitle: '',
-      twitter: '',
-    },
-  };
-
-  handleChange = e => {
-    this.setState({
-      form: {
-        ...this.state.form,
-        [e.target.name]: e.target.value,
-      },
-    });
-  };
-
-  render() {
-    return (
-      <React.Fragment>
+class BadgeNew extends React.Component{
+  render(){
+    return(
+      <div>
+        <Navbar/>
         <div className="BadgeNew__hero">
-          <img className="img-fluid" src={header} alt="Logo" />
+          <img className="img-fluid" src={headers} alt="Logo"/>
         </div>
-
         <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <Badge
-                firstName={this.state.form.firstName}
-                lastName={this.state.form.lastName}
-                twitter={this.state.form.twitter}
-                jobTitle={this.state.form.jobTitle}
-                email={this.state.form.email}
-                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
-              />
-            </div>
-
-            <div className="col-6">
-              <BadgeForm
-                onChange={this.handleChange}
-                formValues={this.state.form}
-              />
-            </div>
+        <div className="row">
+          <div className="col">
+            <Badge
+             firstName="Marco"
+             lastName="Serrate"
+            twiter="serratemarco"
+            jobTitle="Front Enginner"
+            avatarUrl="https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
+            />
           </div>
         </div>
-      </React.Fragment>
-    );
+        </div>
+      </div>
+    )
   }
 }
 
